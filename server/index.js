9fa41590982ec9793353df1b58ad0c050a5a117c
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-20 22:48:35 +0300

    Описание модели данных
    Создание объекта user и его данных

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 const start = async () => {
         try{
-            await sequelize.authenticate() // с помощьюданной функции подключаюсь к БД
+            await sequelize.authenticate() // с помощью данной функции подключаюсь к БД
             await sequelize.sync() //сверка запросов
             app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
         } catch (e) {
