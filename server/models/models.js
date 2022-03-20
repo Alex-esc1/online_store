5bb77b1508189b8d336ac3c67828faa5937f19ea
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-20 22:50:15 +0300

    Описание модели корзины
    Описание объекта корзины

--- server/models/models.js
+++ server/models/models.js
@@ -8,3 +8,11 @@ const User = sequelize.define('user', {
     role: {type: DataTypes.STRING, defaultValue: "USER"},
 })
 
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const BasketDevice = sequelize.define('basket_device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
