0fbf44a1dcb9f9fee8a76b8ef114da658b78e834
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-23 01:43:40 +0300

    Получение ID клиента (проверка)
    Если необходимо получить только ID, то делаю деструктуризацию объекта: http://localhost:5000/api/user/auth?id=5&message=123 - вернет "5"

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,8 +7,8 @@ class UserController {
     }
     async check (reg, res) {
       // res.json ('Проверка работы контроллеров') - для юнит тестов
-      const query = reg.query
-      res.json (query)
+      const {id} = reg.query
+      res.json (id)
     }
 }
 
