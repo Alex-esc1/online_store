b62d8676c8d28e149ea9dc12fb7a8e84504fd9d0
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-23 01:40:07 +0300

    Создаю универсальный обработчик ошибки в контроллере
    Создаю универсальный хендлер. Из строки query мы можем получать параметры строки запроса + проверяю, как это будет работать, вызывая функцию res.json и передаю объект query. Для тестирования использую постмен, и прописываю помимо урл для пользователя, также его данные, пример такой: http://localhost:5000/api/user/auth?id5&message=123

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,6 +7,8 @@ class UserController {
     }
     async check (reg, res) {
       // res.json ('Проверка работы контроллеров') - для юнит тестов
+      const query = reg.query
+      res.json (query)
     }
 }
 
