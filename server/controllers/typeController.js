91a9d69a21c453dc98cc65ee3d28c5424f412d08
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-24 00:32:22 +0300

    typeController.js - прописываю запрос для извлекания типа
    Для проверки создания типа, в Postman прописываю строку http://localhost:5000/api/type/, использую метод POST, данные передаю в формате json

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -3,7 +3,9 @@ const ApiError = require ('../error/ApiError');
 
 class TypeController {
     async create (req, res) {
-        
+        const {name} = req.body
+        const type =  await Type.create({name})
+        return res.json(type)
     }
     async getAll (req, res) {
         
