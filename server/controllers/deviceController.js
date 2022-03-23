bfd435200cd134e4d665fe9b4b2c551294e498f7
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-24 02:33:59 +0300

    Работаем с массивом инфо
    К предыдущему коммиту добавил комментарии к запросам в коде + реализую девайс инфо конструкцию. Важно! formdate приходят ввиде строки, поэтому использую JSON.parse

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,18 +1,28 @@
 const uuid = require ('uuid')
 const path = require ('path');
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require ('../error/ApiError')
 
 
 class DeviceController {
     async create (req, res) {
         try {
-        const {name, price, brandId, typeId, info} = req.body
+        let {name, price, brandId, typeId, info} = req.body
         const {img} = req.files
         let fileName = uuid.v4() + ".jpg"
         img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
         const device = await Device.create ({name, price, brandId, typeId, img: fileName})
+
+        if (info) {
+            info = JSON.parse(info)
+            info.forEach(i =>
+                DeviceInfo.create({
+                    title: i.title,
+                    description: i.description,
+                    deviceId: device.id
+                })
+                )
+        }
 
         return res.json(device)
         } catch (e) {
@@ -20,11 +30,11 @@ class DeviceController {
         }
 
     }
-    async getAll(req, res) {
-        let {brandId, typeId, limit, page} = req.query
-        page = page || 1
-        limit = limit || 9
-        let offset = page * limit - limit
+    async getAll (req, res) {
+        const {brandId, typeId, limit, page} = req.query
+        page = page || 1 // если не указана страница, по дефолту будет первая
+        limit = limit || 9 //по 9-ть устройств на одной странице
+        let offset = page * limit - limit // реализация отступа товаров, по сути 2*9-9 на второй странице получаем остальные 9 устройств
         let devices;
         if (!brandId && !typeId) {
             devices = await Device.findAndCountAll({limit, offset})
