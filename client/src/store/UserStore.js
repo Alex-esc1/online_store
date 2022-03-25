1530a1ec8bccf271bc9862bd1dd755eb3afcc4da
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-26 02:53:21 +0300

    Создаю одноименные GETеры
    Необходимы чтобы получать переменные из состояния. К ним буду обращаться, как к обычным объектам, также использую хук useContext - для создания общих данных, к которым можно обращаться по всей иерархии компонентов

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -13,4 +13,11 @@ export default class UserStore {
     setUser(user) {
         this._user = user
     }
+
+    get isAuth(){
+        return this._isAuth
+    }
+    get user(){
+        return this._user
+    }
 }
