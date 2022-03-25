32ae342c2c724ed0464d156923458628ecb7fa5d
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-26 02:08:29 +0300

    Включаю работу маршрутов
    После описание маршрутов, необходимо их "оживить", прописываю Switch

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route, Redirect, Router} from 'react-router-dom';
+import {authRoutes} from '../routes';
 
 const AppRouter = () => {
+    const isAuth = false
     return (
-        <div>
-            
-        </div>
+        <Switch>
+            {authRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} component={Component} exact/>
+                )}
+        </Switch>
     );
 };
 
