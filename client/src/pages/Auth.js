89bc3fa292247bfb588cbd682cccc154a0c76aa1
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-27 02:07:51 +0300

    Хук useLocation
    В зависимости от того по какой ссылке мы находимся на странице, нам нужно отрисовывать либо страницу регистрации либо авторизации. C помощью хука получим маршрут в строке запроса

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Button, Container, Form, Row} from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import {NavLink} from 'react-router-dom';
-import {REGISTRATION_ROUTE} from '../utils/consts';
+import {NavLink, useLocation} from 'react-router-dom';
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from '../utils/consts';
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
         <Container 
         className='d-flex justify-content-center align-items-center'
         style={{height: window.innerHeight - 54}}
         >
             <Card style={{width: 600}} className="p-5">
-                <h2 className='m-auto'>Авторизация</h2>
+                <h2 className='m-auto'>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
                 <Form className='d-flex flex-column'>
                     <Form.Control
                         className='mt-3'
