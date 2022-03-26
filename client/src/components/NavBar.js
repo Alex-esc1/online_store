92ffb3d8172d57f0fc35276556b907eee7537d7e
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-27 00:32:30 +0300

    Создаю NavLink
    NavLink вместо Navbar.Brand + прописываю часть стилей inline, чтобы  не тратить время на создание файлов со стилями (в данном случае их будет немного, соответственно нет необходимости)

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,15 @@ import {Context} from '../index';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import {NavLink} from 'react-router-dom';
+import {SHOP_ROUTE} from '../utils/consts';
 
 const NavBar = () => {
     const {user} = useContext (Context)
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <NavLink style={{color:'white'}} to={SHOP_ROUTE}> КупиДевайс</NavLink>
             <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
