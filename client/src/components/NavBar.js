90a30b321a5e6fb896bdc6731a4070feaa6ee052
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-27 00:39:24 +0300

    Создаю Button "Авторизации" для NavBar
    Создаю Button для NavBar и прописываю вариант стиля для кнопки outline-light, вместо стандартного

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,6 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import {NavLink} from 'react-router-dom';
 import {SHOP_ROUTE} from '../utils/consts';
+import {Button} from "react-bootstrap";
 
 const NavBar = () => {
     const {user} = useContext (Context)
@@ -13,7 +14,7 @@ const NavBar = () => {
             <Container>
             <NavLink style={{color:'white'}} to={SHOP_ROUTE}> КупиДевайс</NavLink>
             <Nav className="ml-auto" style={{color:'white'}}>
-
+                <Button variant='outline-light'>Авторизация</Button>
             </Nav>
             </Container>
         </Navbar>
