957fdb8ae156f11bae01593cd14e71fbcccdcf7b
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-26 03:07:27 +0300

    Создаю DeviceStore.js
    Создаю типы устройств, mobx

---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,42 @@
+import {makeAutoObservable} from "mobx";
+
+export default class DeviceStore {
+    constructor () {
+        this._types = [
+            {id: 1, name: 'ноутбук'},
+            {id: 5, name: 'смартфоны'},
+            {id: 8, name: 'смартфон'}
+
+        ]
+        this._brands = [
+            {id: 1, name: 'Apple'},
+            {id: 2, name: 'App'}
+
+        ]
+        this._devices = [
+            {id: 5, name: '12 pro', price: 1000, rating: 0, img: `e026b299-6485-40db-9d59-8904a7f60453.jpg`},
+            {id: 21, name: '12yu', price: 1200, rating: 0, img: `ffd8fefb-2eaf-4501-b73f-fb0c2b1e1ba4.jpg`},
+        ]
+        makeAutoObservable(this)
+    }
+
+    setTypes(types) {
+        this._types = types
+    }
+    setBrands(brands) {
+        this._brands = brands
+    }
+    setDevices(devices) {
+        this._devices = devices
+    }
+
+    get types(){
+        return this._types
+    }
+    get brands(){
+        return this._brands
+    }
+    get devices(){
+        return this._devices
+    }
+}
