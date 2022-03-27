0ab0a18c52e24028189907f71e3b57d36665a31e
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-28 02:22:39 +0300

    NavBar useHistory
    "Оживляю" кнопку для перехода в админ панель

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,20 +4,32 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import {NavLink} from 'react-router-dom';
-import {SHOP_ROUTE} from '../utils/consts';
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from '../utils/consts';
 import {Button} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
+import {useHistory} from 'react-router-dom';
 
 const NavBar = observer( () => {
     const {user} = useContext (Context)
+    const history = useHistory()
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color:'white'}} to={SHOP_ROUTE}> КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color:'white'}}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className='ml-2'>Войти</Button>
+                        <Button 
+                            variant={"outline-light"} 
+                            onClick={() => history.push(ADMIN_ROUTE)} 
+                            style={{marginRight:"10px"}}
+                        >
+                            Админ панель</Button>
+                        <Button 
+                            variant={"outline-light"} 
+                            onClick={() => history.push(LOGIN_ROUTE)} 
+                            className='ml-2'
+                        >
+                            Выйти</Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color:'white'}}>
