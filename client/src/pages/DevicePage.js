89ca6a999d332cde32a316fa605fd5999fb5aff1
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-28 02:07:10 +0300

    Блок с кнопкой добавить в корзину
    Реализую блок с кнопкой добавить в корзину на странице товара DevicePage.js

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Container, Image, Row} from 'react-bootstrap';
+import {Button, Card, Col, Container, Image, Row} from 'react-bootstrap';
 import bigStar from '../assets/bigStar.png';
 
 const DevicePage = () => {
@@ -22,7 +22,13 @@ const DevicePage = () => {
                 </Row>
             </Col>
             <Col md={4}>
-            
+                <Card
+                    className='d-flex flex-column align-items-center justify-content-around'
+                    style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray' }}
+                >
+                    <h3>от: {device.price} рублей</h3>
+                    <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                </Card>
             </Col>
             </Row>
         </Container>
