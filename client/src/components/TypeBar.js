c722cd3f92f1eb72250a663ce46067fc8fb8551c
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-27 23:59:14 +0300

    TypeBar for SHOP page
    Для левой панели страницы SHOP, создаю TypeBar компонент c функцией observer, чтобы mobx отслеживал все изменения

---
+++ client/src/components/TypeBar.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+
+const TypeBar = observer( () => {
+    const {device} = useContext(Context)
+    return (
+        <div>
+            
+        </div>
+    );
+});
+
+export default TypeBar;
