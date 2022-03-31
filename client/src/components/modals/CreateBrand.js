dd7b966c3fa53d984f9022b099a239963c6ff0e2
Alex <85848281+Alex-esc1@users.noreply.github.com>
2022-03-31 13:03:07 +0300

    Добавление/удаление данных через админку
    Реализую добавление/удаление через админку в бд + вывод на фронт. Новых: типов, брендов, устройств

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import {Modal, Form, Button} from 'react-bootstrap';
+import React, {useState} from 'react';
+import Modal from "react-bootstrap/Modal";
+import {Button, Form} from "react-bootstrap";
+import {createBrand, createType} from "../../http/deviceAPI";
 
 const CreateBrand = ({show, onHide}) => {
+    const [value, setValue] = useState('')
+
+    const addBrand = () => {
+        createBrand({name: value}).then(data => {
+            setValue('')
+            onHide()
+        })
+    }
     return (
         <Modal
             show={show}
             onHide={onHide}
-            size="lg"
             centered
-            >
+        >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    Добавить бренд
+                    Добавить тип
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form>
                     <Form.Control
-                    placeholder={"Введите название типа"}
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder={"Введите название типа"}
                     />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant={"outline-danger"} onClick={onHide}>Закрыть</Button>
-                <Button variant={"outline-success"} onClick={onHide}>Добавить</Button>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={addBrand}>Добавить</Button>
             </Modal.Footer>
-            </Modal>
+        </Modal>
     );
 };
 
--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import {Modal, Form, Button} from 'react-bootstrap';
-
+import React, {useState} from 'react';
+import Modal from "react-bootstrap/Modal";
+import {Form, Button} from "react-bootstrap";
+import {createType} from "../../http/deviceAPI";
 
 const CreateType = ({show, onHide}) => {
+    const [value, setValue] = useState('')
+
+    const addType = () => {
+        createType({name: value}).then(data => {
+            setValue('')
+            onHide()
+        })
+    }
+
     return (
         <Modal
             show={show}
             onHide={onHide}
-            size="lg"
             centered
-            >
+        >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
                     Добавить тип
@@ -18,15 +27,17 @@ const CreateType = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Form.Control
-                    placeholder={"Введите название типа"}
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder={"Введите название типа"}
                     />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant={"outline-danger"} onClick={onHide}>Закрыть</Button>
-                <Button variant={"outline-success"} onClick={onHide}>Добавить</Button>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={addType}>Добавить</Button>
             </Modal.Footer>
-            </Modal>
+        </Modal>
     );
 };
 
